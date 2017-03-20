2c01c08ca75facca0db4744e0c7cecf5bf958b5a
Ferry van Maurik <ferry.van.maurik@nike.com>
2017-03-20 17:51:04 +0100

    Hello World

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+ var http = require('http')
+ var port = process.env.PORT || 1337;
+ http.createServer(function(req, res) {
+   res.writeHead(200, { 'Content-Type': 'text/plain' });
+   res.end('Hello World\n');
+ }).listen(port);
+
+
+/*
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser')
@@ -29,6 +38,7 @@ app.post('/quotes', function (req, res) {
     console.log(req.body)
 })
 
+*/
 
 
 
